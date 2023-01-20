407847b90eaf3b2a2a197a7e6141763cb181132e
GalitSharon <galit.sharon@il.ey.com>
2023-01-20 13:13:44 +0200

    add app.yaml file

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-// get an API request from end point /related
+// get an API request from endpoint /related
 
 // interact with the datastore
 
 // returns the relevant queries
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
